055bb4b4e271b70b6caf5e3364c7f7bbb409da0d
Joe Mafrici <hello@joemafrici.com>
2022-10-27 02:15:07 -0700

    Fix suto bug

--- okcorraler.sh
+++ okcorraler.sh
@@ -2,7 +2,7 @@
 cd ~/
 
 # Install zsh
-suto apt install zsh
+sudo apt install zsh
 chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
