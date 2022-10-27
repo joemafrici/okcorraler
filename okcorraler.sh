ce6fedfbcdd2e2af0dee87aa2d0f031e3fba2da7
Joe Mafrici <hello@joemafrici.com>
2022-10-27 01:41:23 -0700

    Add commands to install pathogen and plugins, dev tools, and clone my dotfiles

--- okcorraler.sh
+++ okcorraler.sh
@@ -2,3 +2,22 @@
 
 git clone git@github.com:joemafrici/dotfiles.git
 
+# Install dev tools
+sudo apt-get update
+sudo apt-get install build-essential
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install vim airline
+cd ~/.vim/bundle
+git clone https://github.com/vim-airline/vim-airline
+
+# Install vim colors solarized
+cd ~/.vim/bundle
+git clone https://github.com/altercation/vim-colors-solarized
+
+# Install indent guides
+cd ~/.vim/bundle
+git clone https://github.com/nathanaelkane/vim-indent-guides
